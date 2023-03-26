1904822b435f246fd9e64b6c7f63d2d2aa090933
laksor <al.moksud@gmail.com>
2023-03-25 18:09:30 -0700

    added

---
+++ variable-name.js
@@ -0,0 +1,16 @@
+//variable name cannot have quotation
+var 'name' = 'tom';
+
+//variable name cannot be any keywords
+var false = 55;
+var return = true;
+
+//variable name has to be in one line, no space
+var my home = 'new area';
+
+//variable name cannot starts with a number but can ends with number
+var 99club = 13;
+var club66 = 12;
+
+//can not use dash
+var name-dash = 'lala';
---
+++ variable.js
@@ -0,0 +1,11 @@
+//numeric
+var price = 11;
+var age = 12;
+
+//string
+var name = 'shada kala';
+var location = 'new york';
+
+//Boolean
+var serious = true;
+var isSingle = false;
