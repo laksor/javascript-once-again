3a759c47a6b88e1e9359af8cdf4b68ed67b93a1a
laksor <al.moksud@gmail.com>
2023-03-31 08:56:47 -0700

    parseInt & parseFloat

---
+++ conversion.js
@@ -0,0 +1,11 @@
+var price1 = '20';
+var price2 = 30;
+
+//convert string to number parseInt
+var price1Number = parseInt(price1);
+console.log(price1Number);
+
+//using parseFloat
+var gap = '3.55';
+var gapNumber = parseFloat(gap);
+console.log(gapNumber);
