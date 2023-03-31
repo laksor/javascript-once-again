c37e5b87f0f1075c1c9f7169e785a59bac42074d
laksor <al.moksud@gmail.com>
2023-03-31 08:42:31 -0700

    variable value shorthand

--- shorthand.js
+++ shorthand.js
@@ -2,3 +2,17 @@
 var tomatoPrice = 40;
 //variable value update 
 tomatoPrice = 30;
+
+var price1  = 40;
+//changing value
+price1 = price1 + 10;
+console.log(price1);//price1 value changes to 50
+//shorthand changing value
+price1 += 10;
+console.log(price1);
+// adding 1 to the existing
+price1++;
+console.log(price1);
+//--deducting 1 from existing
+price1--;
+console.log(price1);
