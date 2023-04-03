25b58282d6e133e43ef0f384bd000a1b68f8bf46
laksor <al.moksud@gmail.com>
2023-04-03 16:36:09 -0700

    Comparison operator

---
+++ Comparison-operator.js
@@ -0,0 +1,12 @@
+// Comparison operator
+
+console.log(5 > 6);
+console.log(5 < 6);
+console.log(5 == 6);
+console.log(5 >= 6);
+console.log(5 <= 6);
+console.log(5 != 6);
+
+// multiple Comparison operator
+money1 > money2 && result1 > result2 
+money1 || result1 
