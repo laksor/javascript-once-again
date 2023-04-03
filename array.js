0eeb0d2cefc711e99d22fbea2049b815112bcda4
laksor <al.moksud@gmail.com>
2023-04-03 16:14:52 -0700

    index of

--- array.js
+++ array.js
@@ -3,3 +3,14 @@ var randomNames = ['lala', 'kara', 'john'];
 
 // array length
 console.log(friendAge.length);
+
+//array index
+console.log(friendAge[2]);
+//set element by indexing
+var element = friendAge[1];
+friendAge[1] = 77;
+console.log(friendAge);
+
+//index of (position number)
+var position = friendAge.indexOf(11);
+console.log(position);
