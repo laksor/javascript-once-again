aeb498b6b17960793894bdfaca9873309420282a
laksor <al.moksud@gmail.com>
2023-04-07 18:09:32 -0700

    while 1 to 10 number

--- while-loop.js
+++ while-loop.js
@@ -4,3 +4,11 @@ while(num < 5){
     console.log('hello');
     num++; //(hve to change loop variable)
 }
+
+// 1 to 10 number
+
+var number = 1;
+while(number <= 10){
+    console.log(number);
+    number++;
+}
