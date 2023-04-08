88b85aabbee4b6f78dd3277376a983dd111b66dc
laksor <al.moksud@gmail.com>
2023-04-07 17:10:30 -0700

    && , or condition

--- conditional.js
+++ conditional.js
@@ -2,10 +2,25 @@ var phonePrice = 7000;
 var budget = 8000;
 
 if (phonePrice > budget){
-    console.log('dont buy');
+    console.log('do not buy');
 }
 else{
     console.log('buy');
 }
 
 // multiple condition 
+
+var isGraduate = true;
+var salary = 25000;
+var car = 1;
+// && (and operator)
+if(isGraduate == true && salary > 30000 && car >= 1){
+    console.log('wedding');
+}
+// || (or operator)
+else if(isGraduate == true || salary > 30000 || car == 1){
+    console.log('party');
+}
+else{
+    console.log('no wedding');
+}
