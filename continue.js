36dd24e534a6a228308c0ac84c26fa4080b08eb0
laksor <al.moksud@gmail.com>
2023-04-10 22:47:01 -0700

    continue

--- continue.js
+++ continue.js
@@ -1,9 +1,9 @@
-// continue
+// continue(skip)
 var numbers = [20, 25, 55, 66, 77, 4];
 
 for(var i = 0; i < numbers.length; i++){
     var number = numbers[i];
-    if(number > 50){
+    if(number < 50){
         continue;
     }
     console.log(number);
