ef0ccc5f0fa8fc0222c173c08227373ae566dfb7
laksor <al.moksud@gmail.com>
2023-04-13 19:04:27 -0700

    problem

---
+++ average.js
@@ -0,0 +1,11 @@
+//2
+var m = 75.25;
+var b = 65;
+var c  = 80;
+var p = 35.45;
+var bangla = 99.50;
+
+var sum = m+b+c+p+bangla;
+var avg = sum / 5;
+var twoDesimal = avg.toFixed(2);
+console.log(twoDesimal);
---
+++ sum.js
@@ -0,0 +1,8 @@
+//1
+var money = 1000;
+var orange = 400;
+var apple = 300;
+
+var expense = orange + apple;
+var remaining = money - expense;
+console.log(remaining);
